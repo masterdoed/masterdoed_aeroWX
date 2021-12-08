52768af823c7ab987286951b76f07aa7c08fdec8
masterdoed <git@doedspace.de>
2021-12-08 10:09:28 +0100

    inital commit

---
+++ getMetar.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+curl https://tgftp.nws.noaa.gov/data/observations/metar/stations/EDDM.TXT
+curl https://tgftp.nws.noaa.gov/data/observations/metar/decoded/EDDM.TXT
+echo ""
+
+curl https://tgftp.nws.noaa.gov/data/observations/metar/stations/EDMO.TXT
+curl https://tgftp.nws.noaa.gov/data/observations/metar/decoded/EDMO.TXT
+echo ""
+
+curl https://tgftp.nws.noaa.gov/data/observations/metar/stations/EDMA.TXT
+curl https://tgftp.nws.noaa.gov/data/observations/metar/decoded/EDMA.TXT
+echo ""
+
+curl https://tgftp.nws.noaa.gov/data/observations/metar/stations/ETSL.TXT
+curl https://tgftp.nws.noaa.gov/data/observations/metar/decoded/ETSL.TXT
